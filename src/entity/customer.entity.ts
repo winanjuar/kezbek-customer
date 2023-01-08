465b34055de5851b32a3d5a43256937c03da6be7
Sugeng Winanjuar <winanjuar@gmail.com>
2023-01-08 20:38:56 +0700

    increase unit test

--- src/entity/customer.entity.ts
+++ src/entity/customer.entity.ts
@@ -3,6 +3,7 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  Index,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -18,6 +19,7 @@ export class Customer {
   @Column()
   name: string;
 
+  @Index()
   @Column()
   username: string;
 
